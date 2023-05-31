dcf45753e191b02de8b7f689408180f15734dcce
Kota1021 <Kota1021@users.noreply.github.com>
2023-06-01 00:00:24 +0900

    made beautiful UI idea 1

--- Yumemi_iOS_junior_engineer_codecheck/ViewForResearch.swift
+++ Yumemi_iOS_junior_engineer_codecheck/ViewForResearch.swift
@@ -11,30 +11,51 @@ import Alamofire
 struct ViewForResearch: View {
     @State private var output: Result<LuckyPrefacture, AFError>? = nil
     var body: some View {
-        ZStack{
-            Image("4181")
-                .resizable()
-                .scaledToFill()
-                .offset(x: 160, y: 0)//iPhone 14 Proの場合
-                .overlay{
-                    VStack{
-                        Text("Lucky Prefecture")
-                            .foregroundColor(.white)
-                            .fontWeight(.black)
-                            .font(.largeTitle)
-                            .shadow(radius: 5)
-                            .padding(.top, 80)
-                        Spacer()
+        GeometryReader{ geo in
+            ZStack{
+                Image("4181")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .frame(width: geo.size.width, height: geo.size.height)
+                    .offset(x: 160, y: 0)//iPhone 14 Proの場合
+                    .overlay{
+                        VStack{
+                            Text("Lucky Prefecture")
+                                .foregroundColor(.white)
+                                .fontWeight(.black)
+                                .font(.largeTitle)
+                                .shadow(radius: 5)
+                                .padding(.top, 40)
+                            Spacer()
+                        }
                     }
+                
+                VStack{
+                    Spacer()
+                    Group{
+                        HStack{
+                            Text("name")
+                                .padding()
+                            Spacer()
+                        }
+                        HStack{
+                            Text("birthday")
+                                .padding()
+                            Spacer()
+                        }
+                        HStack{
+                            Text("blood type")
+                                .padding()
+                            Spacer()
+                        }
+                        //                    InputView(output: $output)
+                    }.background(Color("Background") )
+                        .padding(.horizontal)
                 }
-            
-                InputView(output: $output)
-                    .background(
-                        Color("Background")
-                            .opacity(0.8)
-                    )
-//                    .border(.pink)
+            }
         }
+        
     }
 }
 
