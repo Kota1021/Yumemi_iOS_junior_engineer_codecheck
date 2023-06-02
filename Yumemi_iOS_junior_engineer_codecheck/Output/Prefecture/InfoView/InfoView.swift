11266b807a72710f70af9d3f8fbadfb88c4f72fc
Kota1021 <Kota1021@users.noreply.github.com>
2023-06-02 20:11:13 +0900

    changed alignment in prefecture view

--- Yumemi_iOS_junior_engineer_codecheck/Output/Prefecture/InfoView/InfoView.swift
+++ Yumemi_iOS_junior_engineer_codecheck/Output/Prefecture/InfoView/InfoView.swift
@@ -13,13 +13,39 @@ struct InfoView: View {
     @Binding var isBreafViewExpanded:Bool
     @Binding var isMapExpanded:Bool
     var body: some View {
+        
         VStack(alignment: .leading){
             HStack{
-                Text(prefecture.name)
-                    .fontWeight(.black)
-                    .font(.system(size: 60))
-                    .shadow(radius: 20)
-                    .padding(10)
+                
+                VStack(alignment: .leading){
+                    
+                    Text(prefecture.name)
+                        .fontWeight(.black)
+                        .font(.system(size: 60))
+                        .shadow(radius: 20)
+                        .padding(10)
+                    
+                    VStack(alignment: .custom){
+                        HStack(spacing: 0){
+                            Text("県庁所在地：　")
+                                .alignmentGuide(.custom) { $0[.trailing] }
+                            Text("\(prefecture.capital)")
+                        }
+                        HStack(spacing: 0){
+                            Text("市民の日：　")
+                                .alignmentGuide(.custom) { $0[.trailing] }
+                            Text(prefecture.citizenDay?.toString() ?? "なし")
+                        }
+                        HStack(spacing: 0){
+                            Text("海：　")
+                                .alignmentGuide(.custom) { $0[.trailing] }
+                            Text(prefecture.hasCoastLine ? "あり":"なし")
+                        }
+                    }
+                    
+                    Text("概要：")
+                        .padding(.top)
+                }
                 
                 AsyncImage(url: prefecture.logoUrl){ image in
                     image
@@ -40,23 +66,10 @@ struct InfoView: View {
                     
                 } placeholder: {
                     ProgressView()
-                }
-            }.frame(height: 100)
+                }.layoutPriority(-1)
+            }
             
-            HStack(spacing: 0){
-                Text("県庁所在地：　")
-                Text("\(prefecture.capital)")
-            }
-            HStack(spacing: 0){
-                Text("市民の日：　")
-                Text(prefecture.citizenDay?.toString() ?? "なし")
-            }
-            HStack(spacing: 0){
-                Text("海：　")
-                Text(prefecture.hasCoastLine ? "あり":"なし")
-            }
-            Text("概要：")
-                .padding(.top)
+            
             VStack{
                 Text( prefecture.brief )
                 HStack{
@@ -81,13 +94,29 @@ struct InfoView: View {
             .padding(.bottom)
             
         }.padding(.horizontal)
-            
+        
     }
 }
 
 
-//struct InfoView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        InfoView()
-//    }
-//}
+struct InfoView_Previews: PreviewProvider {
+    @State static var isDetailDisplayed = false
+    @State static var isMapDisplayed = false
+    static let prefecture = Prefecture(name: "徳島県", brief: "徳島県（とくしまけん）は、日本の四国地方に位置する県。県庁所在地は徳島市。\n※出典: フリー百科事典『ウィキペディア（Wikipedia）』", capital: "徳島市", citizenDay: nil, hasCoastLine: true, logoUrl: URL(string: "https://japan-map.com/wp-content/uploads/tokushima.png")!)
+    
+    
+    static var previews: some View {
+        InfoView(prefecture: prefecture, isBreafViewExpanded: $isDetailDisplayed, isMapExpanded: $isMapDisplayed)
+    }
+}
+
+//cf. https://www.objc.io/blog/2020/03/05/swiftui-alignment-guides/
+struct CustomAlignment: AlignmentID {
+    static func defaultValue(in context: ViewDimensions) -> CGFloat {
+        return context[.leading]
+    }
+}
+
+extension HorizontalAlignment {
+    static let custom: HorizontalAlignment = HorizontalAlignment(CustomAlignment.self)
+}
